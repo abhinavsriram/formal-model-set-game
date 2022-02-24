d3ddc396f2339d25b5ab22495bd6f725117286b9
Ian Acosta <ianacosta@ians-mbp.devices.brown.edu>
2022-02-24 15:38:28 -0500

    generation of cards

--- set_vis.js
+++ set_vis.js
@@ -1,3 +1,13 @@
+function makeShape(shape, color, shading) {
+    if (shape == "Diamond") {
+        return makeDiamond(color, shading)
+    } else if (shape == "Squiggle") {
+        return makeSquiggle(color, shading)
+    } else if (shape == "Oval") {
+        return makeOval(color, shading)
+    }
+}
+
 function makeDiamond(color, shading) {
     e = document.createElement("div")
     e.style.width = "15px"
@@ -6,9 +16,9 @@ function makeDiamond(color, shading) {
     e.style.border = "thin solid " + color
     e.style.margin = "5px"
 
-    if (shading == "solid") {
+    if (shading == "Solid") {
         e.style.backgroundColor = color
-    } else if (shading == "striped") {
+    } else if (shading == "Striped") {
         e.style.background = "repeating-linear-gradient(45deg," + color + "," + color + " 5px,white 5px,white 10px)"
     }
     return e
@@ -22,9 +32,9 @@ function makeOval(color, shading) {
     e.style.border = "thin solid " + color
     e.style.margin = "5px"
 
-    if (shading == "solid") {
+    if (shading == "Solid") {
         e.style.backgroundColor = color
-    } else if (shading == "striped") {
+    } else if (shading == "Striped") {
         e.style.background = "repeating-linear-gradient(45deg," + color + "," + color + " 5px,white 5px,white 10px)"
     }
     return e
@@ -38,9 +48,9 @@ function makeSquiggle(color, shading) {
     e.style.border = "thin solid " + color
     e.style.margin = "5px"
 
-    if (shading == "solid") {
+    if (shading == "Solid") {
         e.style.backgroundColor = color
-    } else if (shading == "striped") {
+    } else if (shading == "Striped") {
         e.style.background = "repeating-linear-gradient(45deg," + color + "," + color + " 5px,white 5px,white 10px)"
     }
     return e
@@ -59,11 +69,36 @@ function makeCard() {
 }
 
 
-function loadCards() {
-
+function convStrToInNum(n) {
+    if (n == "One") {
+        return 1
+    } else if (n == "Two") {
+        return 2
+    } else if (n == "Three") {
+        return 3
+    }
 }
-newCard = makeCard()
-newCard.append(makeDiamond("red","striped"))
-newCard.append(makeOval("purple", "outlined"))
-newCard.append(makeDiamond("green", "solid"))
-div.append(newCard)
+
+function loadCards() {
+    const cards = SetCard
+    for (const ind in cards.tuples()) {
+        const card = cards.tuples()[ind]
+        const cardShape = card.join(shape).toString().slice(0,-1)
+        const cardColor = card.join(color).toString().slice(0,-1)
+        const cardNum = convStrToInNum(card.join(num).toString().slice(0,-1))
+        const cardShading = card.join(shading).toString().slice(0,-1)
+        
+        newCard = makeCard()
+        for (let i = 0; i < cardNum; i++) {
+            newCard.append(makeShape(cardShape, cardColor, cardShading))
+        }
+        div.append(newCard)
+    }
+}
+div.style.display = "flex"
+div.style['flex-direction'] = "row"
+div.style['flex-wrap'] = "wrap"
+div.style.overflow = "scroll"
+div.replaceChildren()
+console.log("HELOOOOOOOOOOOO")
+loadCards()
